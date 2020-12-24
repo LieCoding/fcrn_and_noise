ded9d016bd52d425d8ddb6def49d093843d5182a
LieCoding <914395557@qq.com>
2020-12-24 12:41:25 +0800

    add acc

--- test_net.py
+++ test_net.py
@@ -5,13 +5,8 @@ import numpy as np
 
 tf.reset_default_graph()
 
-epoch =1#迭代次数
-batch_size = 32
-save_step = 50
-
 a = 0.5
 # 用于断点训练时候 保存文件名 
-start_epoch = 130
 
 #选择数据库
 data_dir = 'dataset/val/image'
@@ -27,6 +22,23 @@ image = tf.placeholder(tf.float32, [None, h, w, 3])
 noise_image = tf.placeholder(tf.float32, [None,h, w, 3])
 label = tf.placeholder(tf.float32, [None, h, w, 1]) 
 
+def get_acc(im1,im2):
+ 
+    # print(im1.shape)
+    # 交集 、并集
+    intersection ,union= 0.,0.
+
+    for i,j in zip(im1,im2):
+        for m,n in zip(i,j):
+            if m!=0 and n!=0:
+                intersection+=1
+            if m!=0 or n!=0:
+                union+=1
+#     print(intersection,union,intersection/union)
+
+    return intersection/union
+
+
 #获取编码器网络输出
 real_feature,D1,D2,D3 = netStore.net_encoder1(image)
 #获取噪声发生器网络输出
@@ -38,79 +50,53 @@ real_map  = netStore.net_decoder(real_feature,D1,D2,D3,a)
 fake_map  = netStore.net_decoder(real_feature,D1,D2,D3,a,noise_feature)
 
 
-# 解码器的损失函数：
-loss_decoder = tf.reduce_mean(((real_map-label)**2)+((fake_map-label)**2))
-# 噪声发生器的损失：
-loss_G = tf.reduce_mean(-1*(fake_map-label)**2)
-# 编码器的损失函数：
-loss_encoder = tf.reduce_mean(((real_map-label)**2)+((fake_map-label)**2))
-# 总损失
-total_loss = loss_decoder+loss_G+loss_encoder
-#设置学习率
-global_step = tf.Variable(0, trainable=False)  
-lr = tf.train.exponential_decay(0.001,
-                                global_step,
-                                1000,
-                                0.9,
-                                staircase=True)#指数衰减
-#设置优化器
-optimizer = tf.train.AdamOptimizer(lr).minimize(total_loss, global_step=global_step)
-
-# optimizer_G = tf.train.AdamOptimizer(lr).minimize(loss_G, global_step=global_step,var_list=G_vars)
-
-
 saver = tf.train.Saver(tf.global_variables(), max_to_keep=1)
-
+acc,im_num = 0.,0.
 with tf.Session() as sess:
     ckpt = tf.train.get_checkpoint_state('./model/')
     if ckpt and tf.train.checkpoint_exists(ckpt.model_checkpoint_path):
         saver.restore(sess, ckpt.model_checkpoint_path)
         print('Import models successful!')
     else:
-        sess.run(tf.global_variables_initializer())
         print('模型加载失败')
         sys.exit()
-    for i in range(epoch):
-        start = time.time()
-        index = 0
-        for j in range(int(len(images)/batch_size)):
-            if index + batch_size > len(images):
-                list_temp = images[index:len(images)]
-            else:
-                list_temp = images[index:index+batch_size]
-            
-            #获取图片数据 label 加入了噪声的图片
-            image_data, real_label_data ,noise_image_data = reader.read_data(data_dir,list_temp,output_size=(w, h))
+        
+    start = time.time()
+    
+    list_temp = images
+    
+    #获取图片数据 label 加入了噪声的图片
+    image_data, real_label_data ,noise_image_data = reader.read_data(data_dir,list_temp,output_size=(w, h))
+    #------------训练网络-------------
+    feeds = {image: image_data, label: real_label_data,noise_image:noise_image_data}
 
-            #------------训练网络-------------
-            feeds = {image: image_data, label: real_label_data,noise_image:noise_image_data}
-            
-            input_image, input_noise_image,output_real_map,output_fake_map, input_label= sess.run([image,noise_image,real_map,
-                                                                                                   fake_map,label], feeds)
-            
-#             _,input_image, input_noise_image,output_real_map,output_fake_map, input_label,\
-#             decoder_loss_output, encoder_loss_output,G_loss_output, total_loss_output,\
-#             learningrate = sess.run([optimizer,image,noise_image,real_map, fake_map,
-#                                      label,loss_decoder,loss_encoder,loss_G,total_loss,lr], feeds)
-            
-            index += batch_size 
-            
-            
-            
-#         print('epoch:{} total_loss:{} decoder_loss:{} encoder_loss:{} G_loss:{} lr:{}'.format(i+start_epoch,
-#                                                                                               total_loss_output,decoder_loss_output, 
-#                                                                                            encoder_loss_output,G_loss_output, 
-#                                                                                               learningrate))
+    input_image, input_noise_image,output_real_map,output_fake_map, input_label= sess.run([image,noise_image,real_map,
+                                                                                           fake_map,label], feeds)
+
+
+    end = time.time()
+    print("运行时间:%.2f秒"%(end-start),'正在保存图片 计算准确率------')
+
+
+    for image_id in range(len(input_image)):
+        cv2.imwrite('./test_record/' + str(image_id)  + "_" +  '_image' + '.png' , input_image[image_id])
+        cv2.imwrite('./test_record/' + str(image_id)  + "_" +  'input_noise_image' + '.png' , input_noise_image[image_id])
+        cv2.imwrite('./test_record/' + str(image_id)  + "_" +  'label' + '.png' , input_label[image_id])
+        cv2.imwrite('./test_record/' + str(image_id) +  "_" +  'output_real_map' + '.png' , output_real_map[image_id])
+        cv2.imwrite('./test_record/' + str(image_id) + "_" +  'output_fake_map' + '.png' , output_fake_map[image_id])
+
+            #计算准确率cv2.cvtColor(im1,cv2.COLOR_BGR2GRAY)
+        im1 = cv2.imread('./test_record/' + str(image_id) +  '_label' + '.png')
+        im2 = cv2.imread('./test_record/' + str(image_id)+ '_output_real_map' + '.png')
         
-        end = time.time()
-        print("运行时间:%.2f秒"%(end-start))
-        
-        for image_id in range(len(input_image)):
-                cv2.imwrite('./test_record/' + str(i+image_id) + str(j) + "_" +  '_image' + '.png' , input_image[image_id])
-                cv2.imwrite('./test_record/' + str(i+image_id) + str(j) + "_" +  'input_noise_image' + '.png' , input_noise_image[image_id])
-                cv2.imwrite('./test_record/' + str(i+image_id) + str(j) + "_" +  '_label' + '.png' , input_label[image_id])
-                cv2.imwrite('./test_record/' + str(i+image_id) + str(j)+ "_" +  'output_real_map' + '.png' , output_real_map[image_id])
-                cv2.imwrite('./test_record/' + str(i+image_id) + str(j)+ "_" +  'output_fake_map' + '.png' , output_fake_map[image_id])
-                
+        im1=cv2.cvtColor(im1,cv2.COLOR_BGR2GRAY)
+        im2=cv2.cvtColor(im2,cv2.COLOR_BGR2GRAY)
+
+        acc_id = get_acc(im1,im2)
+        print(acc_id)
+        acc+=acc_id
+        im_num+=1
+    acc /= im_num    
+    print(acc*100,'%','im_num:',im_num)
     print("Done!")
     
